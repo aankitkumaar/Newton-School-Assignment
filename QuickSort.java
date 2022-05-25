32a31aa312e9dc7f17dab2f2600778f318561368
Ankit kumar <48665804+aankitkumaar@users.noreply.github.com>
2022-05-25 14:58:23 +0530

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,47 +1,39 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
-public class QuickSort {
-
-    public static int[] quickSort(int arr[], int l, int r){
-        if (l >= r)
-            return arr;
-        int pivot = arr[r];
-        int pi = partitionIndex(arr, pivot, l, r);
-        quickSort(arr, l, pi - 1);
-        quickSort(arr, pi + 1, r);
-        return arr;
-    }
-
-    public static int partitionIndex(int[] arr, int pivot, int l, int r) {
-        int i = l;
-        int j = l;
-        while (i <= r) {
-            if (arr[i] <= pivot) {
-                swap(arr, i, j);
-                i++;
-                j++;
-            } else {
-                i++;
-            }
-        }
-        return j - 1;
-    }
-
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
+public class quicksort
+{
+    public static void main(String[] args)
+    {
+        int arr[] = {1, 88, 4, 0, 56, 7};
+        int n = arr.length;
         quickSort(arr,0,n-1);
         System.out.println(Arrays.toString(arr));
-
+        
     }
+    public static void quickSort(int arr[], int begin, int end) {
+    if (begin < end) {
+        int partitionIndex = partition(arr, begin, end);
+        quickSort(arr, begin, partitionIndex-1);
+        quickSort(arr, partitionIndex+1, end);
+    }
+}
+    public static int partition(int arr[], int begin, int end) {
+    int pivot = arr[end];
+    int i = (begin-1);
+
+    for (int j = begin; j < end; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+
+            int swapTemp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = swapTemp;
+        }
+    }
+    int swapTemp = arr[i+1];
+    arr[i+1] = arr[end];
+    arr[end] = swapTemp;
+
+    return i+1;
+}
 }
